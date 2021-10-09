affa8c6cb656538ceb85ccea7eef2caa99fc7df0
Wael Moharram <wael.moharram@gmail.com>
2021-10-09 13:30:14 +0200

    discount

--- resources/views/dashboard/bills/sale_in/_form_details.blade.php
+++ resources/views/dashboard/bills/sale_in/_form_details.blade.php
@@ -13,8 +13,8 @@
 </div>
 @can('discount sales')
     <div class="form-group  col-md-12">
-        <label for="amount"> الخصم ان وجد  </label>
-        {{Form::text('amount',0,['class'=>'form-control mb-2','id'=>'amount'])}}
+        <label for="discount"> الخصم ان وجد  </label>
+        {{Form::text('discount',0,['class'=>'form-control mb-2','id'=>'amount'])}}
         {{input_error($errors,'amount')}}
     </div>
 @endcan
