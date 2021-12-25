c50a01beb593350b69731febdf53219d195de66b
Wael Moharram <wael.moharram@gmail.com>
2021-12-25 10:43:42 +0200

    categories

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -4,7 +4,9 @@
 
 @section('content_header')
     <h1>مستخدمين النظام</h1>
-    <a href="#" class="btn btn-info float-right">اضافة جديد</a>
+    @can('index users')
+        <a href="{{route('users.create')}}" class="btn btn-info float-right">اضافة جديد</a>
+    @endcan
 @stop
 
 @section('content')
