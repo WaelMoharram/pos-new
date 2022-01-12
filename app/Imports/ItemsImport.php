ab650faf3789a7d539272113376a93a3ebd1673f
Wael Moharram <wael.moharram@gmail.com>
2022-01-12 22:36:26 +0200

    excel

---
+++ app/Imports/ItemsImport.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Category;
+use App\Models\Item;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class ItemsImport implements ToCollection
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row){
+            $category1=null;
+            $category2=null;
+            $category3=null;
+            if ($row[4] != null || $row[4] != ''){
+                $category1 = Category::where('name',$row[4])->first();
+                $category1 = $category1->id;
+                if (!$category1){
+                    $category1 = Category::create([
+                        'name'=>'',
+                        'image'=>'uploads/images/164197706793default_product.png',
+                        'upper_id'=>null
+                    ]);
+                    $category1 = $category1->id;
+                }
+            }
+
+            if ($row[3] != null || $row[3] != ''){
+                $category2 = Category::where('name',$row[3])->first();
+                $category2 = $category2->id;
+                if (!$category2){
+                    $category2 = Category::create([
+                        'name'=>'',
+                        'image'=>'uploads/images/164197706793default_product.png',
+                        'upper_id'=>$category1
+                    ]);
+                    $category2 = $category2->id;
+                }
+            }
+
+            if ($row[2] != null || $row[2] != ''){
+                $category3 = Category::where('name',$row[2])->first();
+                $category3 = $category3->id;
+                if (!$category3){
+                    $category3 = Category::create([
+                        'name'=>'',
+                        'image'=>'uploads/images/164197706793default_product.png',
+                        'upper_id'=>$category2
+                    ]);
+                    $category3 = $category3->id;
+                }
+            }
+            $item = Item::create([
+                'name'=>$row[0],
+                'barcode'=>'',
+                'code'=>'',
+                'category_id'=>$category3,
+                'brand_id'=>1,
+                'price'=>$row[1],
+                'buy_price'=>$row[1]
+            ]);
+
+            $item->fill([
+                    'barcode'=>$item->id,
+                    'code'=>$item->id,
+                    ])->save();
+        }
+
+    }
+}
