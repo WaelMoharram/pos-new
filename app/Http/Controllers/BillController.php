76c06b7ece76f46d7dc6673a86436805062170d3
Wael Moharram <wael.moharram@gmail.com>
2022-01-16 13:44:03 +0200

    store relations

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -30,7 +30,7 @@ class BillController extends Controller
     public function index(Request $request)
     {
         $bills = Bill::where('type',$request->type)->get();
-        if (auth()->user()->store){
+        if (count(auth()->user()->store) > 0){
             $bills = Bill::where('type',$request->type)->where('sales_man_id',auth()->id())->get();
 
         }
