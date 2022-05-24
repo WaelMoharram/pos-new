fcfbc084cddb2b7284f328c4291fa780067795e9
Wael Moharram <wael.moharram@gmail.com>
2022-05-24 11:22:21 +0200

    barcode

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -1,6 +1,6 @@
 @foreach($details as $one)
     <img src="data:image/png,{{ \Milon\Barcode\DNS1D::getBarcodePNG('4', 'C39+') }}" alt="barcode"   />
-    <p>{{$details->item->barcode}}</p>
+    <p>{{$one->item->barcode}}</p>
     @if(checkEvenOdd(($loop->index + 1)) == 'Even' )
         <p style="page-break-after: always;">&nbsp;</p>
         <p style="page-break-before: always;">&nbsp;</p>
