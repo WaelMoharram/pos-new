011537ea9c7d7d6b0be603798a46ba03302f356c
Wael Moharram <wael.moharram@gmail.com>
2022-05-26 17:34:04 +0200

    barcode

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -2,14 +2,16 @@
     @for($i=0;$i<$one->amount;$i++)
     <div style="width: 3.8cm; height: 1.2cm;">
     <img style="margin: 4px;" src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->id, 'C39',1.4,45,array(1,1,1), true) }}" alt="barcode"   />
-    </div><div style="page-break-after: always;">&nbsp;</div>
-    <div style="page-break-before: always;">&nbsp;</div>
+    </div>
+{{--    <div style="page-break-after: always;">&nbsp;</div>--}}
+{{--    <div style="page-break-before: always;">&nbsp;</div>--}}
     {{--<p style="page-break-before: always;">&nbsp;</p>
 
     {{--    <p>{{$one->item->barcode}}</p>--}}
-{{--    @if(checkEvenOdd(($count++)) == 'Even' )--}}
-{{--        <div style="page-break-after: always;">&nbsp;</div>--}}
-{{--    @endif--}}
+    @if(checkEvenOdd(($count++)) == 'Even' )
+        <div style="page-break-after: always;">&nbsp;</div>
+        <div style="page-break-before: always;">&nbsp;</div>
+    @endif
     @endfor
 @endforeach
 {{--<p>1234</p>--}}
