bd4f2c9284afe6cb044fac59a6d39bfbd1fdadb5
Wael Moharram <wael.moharram@gmail.com>
2022-05-26 17:28:33 +0200

    barcode

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -3,7 +3,10 @@
     <div style="width: 3.8cm; height: 1.2cm;">
     <img style="margin: 4px;" src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->id, 'C39',1.4,45,array(1,1,1), true) }}" alt="barcode"   />
     </div><div style="page-break-after: always;">&nbsp;</div>
-{{--    <p>{{$one->item->barcode}}</p>--}}
+    <div style="page-break-before: always;">&nbsp;</div>
+    {{--<p style="page-break-before: always;">&nbsp;</p>
+
+    {{--    <p>{{$one->item->barcode}}</p>--}}
 {{--    @if(checkEvenOdd(($count++)) == 'Even' )--}}
 {{--        <div style="page-break-after: always;">&nbsp;</div>--}}
 {{--    @endif--}}
