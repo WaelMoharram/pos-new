e6bdd492b97859065b6fa870943949ad2677a0b2
Wael Moharram <wael.moharram@gmail.com>
2022-05-26 17:19:10 +0200

    barcode

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -2,11 +2,11 @@
     @for($i=0;$i<$one->amount;$i++)
     <div style="width: 3.8cm; height: 1.2cm;">
     <img style="margin: 4px;" src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->id, 'C39',1.4,45,array(1,1,1), true) }}" alt="barcode"   />
-    </div>
+    </div><div style="page-break-after: always;">&nbsp;</div>
 {{--    <p>{{$one->item->barcode}}</p>--}}
-    @if(checkEvenOdd(($count++)) == 'Even' )
-        <div style="page-break-after: always;">&nbsp;</div>
-    @endif
+{{--    @if(checkEvenOdd(($count++)) == 'Even' )--}}
+{{--        <div style="page-break-after: always;">&nbsp;</div>--}}
+{{--    @endif--}}
     @endfor
 @endforeach
 {{--<p>1234</p>--}}
