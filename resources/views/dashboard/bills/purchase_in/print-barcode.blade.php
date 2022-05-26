a9f459c9d413f03d2d64ceb00ce4d6048d9886e6
Wael Moharram <wael.moharram@gmail.com>
2022-05-26 17:38:35 +0200

    barcode

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -1,7 +1,7 @@
 @foreach($details as $one)
     @for($i=0;$i<$one->amount;$i++)
     <div style="width: 3.8cm; height: 1.2cm;">
-    <img style="margin: 4px;" src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->id, 'C39',1.4,45,array(1,1,1), true) }}" alt="barcode"   />
+    <img  src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->id, 'C39',1.4,45,array(1,1,1), true) }}" alt="barcode"   />
     </div>
 {{--    <div style="page-break-after: always;">&nbsp;</div>--}}
 {{--    <div style="page-break-before: always;">&nbsp;</div>--}}
