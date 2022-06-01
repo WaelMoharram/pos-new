d1bfad757430ffad8902ed96ed169f5c244a032b
Wael Moharram <wael.moharram@gmail.com>
2022-06-01 16:03:41 +0200

    show errors

--- resources/views/components/tool/datatable.blade.php
+++ resources/views/components/tool/datatable.blade.php
@@ -40,11 +40,11 @@
 <script>
 
     $(() => {
-        $('#{{ $id }}').DataTable( {
+        $('#{{ $id }}').DataTable({
             "language": {
-                "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/ar.json"
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
             }
-        });
+        } );
     })
 
 </script>
