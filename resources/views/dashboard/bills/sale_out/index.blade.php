1f072cfccec414020f39266063166d7d4005cacb
Wael Moharram <wael.moharram@gmail.com>
2022-06-02 17:07:38 +0200

    sale status check

--- resources/views/dashboard/bills/sale_out/index.blade.php
+++ resources/views/dashboard/bills/sale_out/index.blade.php
@@ -10,7 +10,7 @@
     {{-- Setup data for datatables --}}
     @if(request()->type == 'sale_out')
         @can('add sales')
-            @if (auth()->user()->has('store') && auth()->user()->store->is_pos == 1)
+            @if (auth()->user()->store != null && auth()->user()->store->is_pos == 1)
     @component('partials.buttons._add_sale_bill_button',[
                                             'route' => route('bills.store',['type'=>'sale_out']) ,
                                             'tooltip' => 'اضافة',
