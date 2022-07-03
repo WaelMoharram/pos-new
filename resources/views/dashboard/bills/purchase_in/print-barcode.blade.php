73e4a9aa19b8d4b947d319b14295939f0231e961
Wael Moharram <wael.moharram@gmail.com>
2022-07-03 15:26:14 +0200

    Revert barcode cycle to old

--- resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
+++ resources/views/dashboard/bills/purchase_in/print-barcode.blade.php
@@ -9,20 +9,22 @@
     }
 </style>
 @foreach($details as $one)
-    <div class="print">
-        {!!  \Milon\Barcode\DNS1D::getBarcodeHTML($one->item->barcode, 'C128',true) !!}
-        <div style="width: 135px;font-size: 12px;">
-            <span>{{$one->item->barcode}}</span>
+    @for($i=0;$i<$one->amount;$i++)
+        <div class="print">
+            {!!  \Milon\Barcode\DNS1D::getBarcodeHTML($one->item->barcode, 'C128',true) !!}
+            <div style="width: 135px;font-size: 12px;">
+                <span>{{$one->item->barcode}}</span>
+            </div>
+            <div style="width: 135px;font-size: 12px;">
+                <span>{{$one->item->name}}</span>
+            </div>
+
+            {{--    <img  src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->barcode, 'C128',1.2,30,array(1,1,1), true) }}" alt="barcode"   />--}}
         </div>
-        <div style="width: 135px;font-size: 12px;">
-            <span>{{$one->item->name}}</span>
-        </div>
-
-{{--    <img  src="data:image/png;base64,{{ \Milon\Barcode\DNS1D::getBarcodePNG($one->item->barcode, 'C128',1.2,30,array(1,1,1), true) }}" alt="barcode"   />--}}
-    </div>
-    <div style="page-break-before: always; height: 1mm;">&nbsp;</div>
+        <div style="page-break-before: always; height: 1mm;">&nbsp;</div>
 
 
+    @endfor
 @endforeach
 {{--<p>1234</p>--}}
 {{--<p style="page-break-after: always;">&nbsp;</p>--}}
