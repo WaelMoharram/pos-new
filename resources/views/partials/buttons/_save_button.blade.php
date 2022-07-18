726ddd2093fa894b01ba8cf85e0a40db23cc6d6f
Wael Moharram <wael.moharram@gmail.com>
2022-07-18 16:59:34 +0200

    disabled on submit

--- resources/views/partials/buttons/_save_button.blade.php
+++ resources/views/partials/buttons/_save_button.blade.php
@@ -1,4 +1,11 @@
 <div class="col-12">
-    <button type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light">حفظ</button>
+    <button id="target" type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light">حفظ</button>
     <button type="reset" class="btn btn-outline-warning mr-1 mb-1 waves-effect waves-light">الفاء</button>
 </div>
+
+
+<script>
+    $( "#target" ).click(function() {
+        $( "#target" ).addClass( "disabled" );
+    });
+</script>
