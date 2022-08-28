a92208e1afaf9b23fe7f391bed46bd7ac2c4de3b
Wael Moharram <wael.moharram@gmail.com>
2022-08-28 14:44:35 +0200

    Reset button

--- resources/views/partials/buttons/_save_button.blade.php
+++ resources/views/partials/buttons/_save_button.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12">
     <button id="target" type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light">حفظ</button>
-    <button type="reset" class="btn btn-outline-warning mr-1 mb-1 waves-effect waves-light">الفاء</button>
+    <button type="reset" class="btn btn-outline-warning mr-1 mb-1 waves-effect waves-light">اعادة تعيين</button>
 </div>
 
 
