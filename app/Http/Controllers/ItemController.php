dc6dcffefd1a8d2421d8f3b699dd76ff9cf2fed2
Wael Moharram <wael.moharram@gmail.com>
2022-08-28 16:50:44 +0200

    Reset button

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -19,7 +19,7 @@ class ItemController extends Controller
      */
     public function __construct()
     {
-       $this->middleware(['auth','notsales']);
+       $this->middleware(['auth']);
     }
 
     /**
