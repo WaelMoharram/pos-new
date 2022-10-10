19c341bfa9c07e07f94e3eba7b5112ecf0725371
Wael Moharram <wael.moharram@gmail.com>
2022-10-10 13:56:44 +0200

    default units

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -35,7 +35,7 @@
             <tr>
                 <td>{!! substr(str_repeat(0, 5).($loop->index +1), - 5); !!}</td>
                 <td>@if($row->image)<img src="{!! url('/').'/'.$row->image !!}" style="width: 100px; height: 100px;">@else <img src="{!! url('no.png') !!}" style="width: 100px; height: 100px;"> @endif</td>
-                <td>{!! $row->name !!}</td>
+                <td><a href="{{route('sales-men.report',$row->id)}}">{!! $row->name !!}</a></td>
                 <td>{!! $row->username !!}</td>
                 <td>{!! $row->email !!}</td>
                 <td>{!! $row->mobile !!}</td>
