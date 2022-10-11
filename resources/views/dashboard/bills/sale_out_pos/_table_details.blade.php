89ae491180137152d01d4f871a0780b384f7e486
Wael Moharram <wael.moharram@gmail.com>
2022-10-11 12:49:16 +0200

    default units

--- resources/views/dashboard/bills/sale_out_pos/_table_details.blade.php
+++ resources/views/dashboard/bills/sale_out_pos/_table_details.blade.php
@@ -42,8 +42,6 @@
                             {{$detail->total}}
                         </td>
                         <td><span class="">
-                                {{$detail}}
-                                {{\App\Models\Bill::where('item_id',$detail->item_id)->where('unit_id',$detail->unit_id)->where('bill_id',$detail->bill_id)->first()}}
                                 @if(\App\Models\Bill::where('item_id',$detail->item_id)->where('unit_id',$detail->unit_id)->where('bill_id',$detail->bill_id)->first()->collected_at  == null)
                                     @component('partials.buttons._delete_button',[
                                         'id'=>$detail->id,
