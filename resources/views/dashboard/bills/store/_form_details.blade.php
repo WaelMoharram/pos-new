f411713ce3cc8527ea80bafbd0091312fb06682d
Wael Moharram <wael.moharram@gmail.com>
2022-11-03 15:34:54 +0200

    delete bill and return amount to stores

--- resources/views/dashboard/bills/store/_form_details.blade.php
+++ resources/views/dashboard/bills/store/_form_details.blade.php
@@ -12,6 +12,11 @@
     {{input_error($errors,'amount')}}
 </div>
 <div class="form-group col-md-12">
+    <label for="unit_id"> اختر الوحدة </label>
+    {{Form::select('unit_id',[] ,null,['class'=>'form-control mb-2','id'=>'unit_id'])}}
+    {{input_error($errors,'unit_id')}}
+</div>
+<div class="form-group col-md-12">
 <button type="submit" class="col-md-12 btn btn-primary mr-1 mb-1 waves-effect waves-light">اضافة</button>
 </div>
 
