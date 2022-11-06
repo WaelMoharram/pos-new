08d5bfaf224f5448f09953ec6e5101a1bdd7c3f4
Wael Moharram <wael.moharram@gmail.com>
2022-11-06 16:22:38 +0200

    units in print invoice details

--- resources/views/dashboard/items/index.blade.php
+++ resources/views/dashboard/items/index.blade.php
@@ -58,13 +58,8 @@
 
                 <td>
                     @foreach(\App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->get() as $unit)
-                        {{$amount}} 11111
-                    {{$amount * $unit->ratio}}********
-                        @php($amount = $amount * $unit->ratio)
-                        {{$amount}} 22222
-                        @if(getRound((float)$amount) != 0)
-                            {{getRound($amount)}} 33333
-                            <span {{tooltip($unit->name)}}>{{getRound($amount)}}</span> @if(($loop->index +1) != \App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->count()) - @endif
+                        @if(getRound((float)($amount * $unit->ratio)) != 0)
+                            <span {{tooltip($unit->name)}}>{{getRound((float)($amount * $unit->ratio))}}</span> @if(($loop->index +1) != \App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->count()) - @endif
                             @php($amount = getFrachtion(\App\Models\ItemStore::where('item_id',$row->id)->sum('amount')))
                         @endif
                     @endforeach
