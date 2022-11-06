345abe29666a8dd9894c3ff7f9fd05cffc8141e7
Wael Moharram <wael.moharram@gmail.com>
2022-11-06 11:08:14 +0200

    units in print invoice details

--- resources/views/dashboard/items/index.blade.php
+++ resources/views/dashboard/items/index.blade.php
@@ -35,12 +35,14 @@
     <x-adminlte-datatable id="table1" :heads="$heads" striped hoverable with-buttons>
         @foreach($items as $row)
             <tr>
+
 {{--                <td>{!! substr(str_repeat(0, 5).($loop->index +1), - 5); !!}</td>--}}
                 <td>@if($row->image)<img src="{!! url('/').'/'.$row->image !!}" style="width: 100px; height: 100px;">@else <img src="{!! url('no.png') !!}" style="width: 100px; height: 100px;"> @endif</td>
                 <td>{!! $row->name !!}</td>
                 @php($amount = \App\Models\ItemStore::where('item_id',$row->id)->sum('amount'))
 
                 <td>
+                    {{            \App\Models\ItemStore::where('item_id',$row->id)->sum('amount')}} ***
                     <br>
                     <br>
                     @foreach(\App\Models\Unit::where('item_id',$row->id)->get() as $unit)
