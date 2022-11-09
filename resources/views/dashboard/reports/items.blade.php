9efa78dc70430027a1f8a13d64fb91d029ff44b3
Wael Moharram <wael.moharram@gmail.com>
2022-11-09 15:51:49 +0200

    Validate unit delete

--- resources/views/dashboard/reports/items.blade.php
+++ resources/views/dashboard/reports/items.blade.php
@@ -78,7 +78,6 @@
                     @foreach(\App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->get() as $unit)
 
                         <span {{tooltip($unit->name)}}>{{getRound(($amount * (float)$unit->ratio))}}</span> @if(($loop->index +1) != \App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->count()) - @endif
-                    {{$amount * (float)$unit->ratio}}
                         @php($amount = getFrachtion(getRound(($amount * (float)$unit->ratio))))
 
                     @endforeach
