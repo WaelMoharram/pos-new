8352282c1689e9aad08bc242194f0fb27bb0b66e
Wael Moharram <wael.moharram@gmail.com>
2022-11-13 12:55:54 +0200

    وجد

--- resources/views/dashboard/bills/store/_form.blade.php
+++ resources/views/dashboard/bills/store/_form.blade.php
@@ -11,13 +11,13 @@
 {{-- ############# Supplier #############--}}
 <div class="form-group py-1 col-md-6">
     <label for="model_id"> مخزن الصرف  </label>
-    {{Form::select('store_from_id',\App\Models\Store::pluck('name','id') ,null,['disabled','class'=>'form-control mb-2','id'=>'store_from_id'])}}
+    {{Form::select('store_from_id',\App\Models\Store::get()->pluck('select_name','id') ,null,['disabled','class'=>'form-control mb-2','id'=>'store_from_id'])}}
     {{input_error($errors,'store_from_id')}}
 </div>
 {{-- ############# Store #############--}}
 <div class="form-group py-1 col-md-6">
     <label for="store_id"> مخزن الاستلام  </label>
-    {{Form::select('store_to_id',\App\Models\Store::pluck('name','id') ,null,['class'=>'form-control mb-2','id'=>'store_to_id'])}}
+    {{Form::select('store_to_id',\App\Models\Store::get()->pluck('select_name','id') ,null,['disabled','class'=>'form-control mb-2','id'=>'store_to_id'])}}
     {{input_error($errors,'store_to_id')}}
 </div>
 {{-- ############# Bill Code #############--}}
