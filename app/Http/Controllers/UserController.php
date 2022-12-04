b6580e1d1f125ec42bc21e4d90b11e83518b7ba6
Wael Moharram <wael.moharram@gmail.com>
2022-12-04 15:21:24 +0200

    log user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -143,11 +143,11 @@ class UserController extends Controller
         $user = User::find($id);
         $oldUser= User::find($id);
 
+        $oldUser['role'] = $oldUser->roles->first()->name;
         $user->fill($requests)->save();
         $user->syncRoles($request->role);
         $logUser = $user;
         $logUser['role'] = $user->roles->first()->name;
-        $oldUser['role'] = $oldUser->roles->first()->name;
 
         //dd(['old'=>$oldUser,'attributes'=>$logUser]);
         activity()->withProperties(['old'=>$oldUser,'attributes'=>$logUser])
