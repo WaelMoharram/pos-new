0058f2ba6ec481450806f1e7ec4c6ae2e07003bb
Wael Moharram <wael.moharram@gmail.com>
2023-01-01 10:52:17 +0200

    Update index.blade.php

--- resources/views/dashboard/bills/sale_out_pos/index.blade.php
+++ resources/views/dashboard/bills/sale_out_pos/index.blade.php
@@ -151,6 +151,10 @@
             });
         });
     </script>
-
+<script>
+        $('.select2').select2({
+            dir:'rtl',
+        });
+    </script>
     @include('sweetalert::alert')
 @stop
