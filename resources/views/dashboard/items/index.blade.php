47b4de32d6634427f4d573aa0532ad5b660cb09c
Wael Moharram <wael.moharram@gmail.com>
2023-03-13 14:36:26 +0200

    upload

--- resources/views/dashboard/items/index.blade.php
+++ resources/views/dashboard/items/index.blade.php
@@ -16,7 +16,7 @@
             <div class="modal-dialog modal-dialog-scrollable" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title" id="myModalLabel1">حذف</h4>
+                        <h4 class="modal-title" id="myModalLabel1">رفع ملف الاسعار</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
