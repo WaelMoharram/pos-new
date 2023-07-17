32d510f2efd6349c88346adec1305179a62d56fc
Wael Moharram <wael.moharram@gmail.com>
2023-07-17 16:39:11 +0300

    filters

--- resources/views/dashboard/reports/store-card.blade.php
+++ resources/views/dashboard/reports/store-card.blade.php
@@ -169,7 +169,7 @@ $total=[];
 
                         <td>{{number_format($detail->amount / ($detail->unit ?$detail->unit->ratio : 1) , 2)}}</td>
                     <td>{{number_format($detail->item->buy_price , 2)}}</td>
-                    <td>{{(number_format((($detail->amount / ($detail->unit ?$detail->unit->ratio : 1)) * $detail->item->buy_price ), 2)}}</td>
+                    <td>{{number_format((($detail->amount / ($detail->unit ?$detail->unit->ratio : 1)) * $detail->item->buy_price ), 2)}}</td>
                     <td>0</td>
                     <td>0</td>
                     <td>0</td>
