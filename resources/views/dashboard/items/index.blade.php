73ba24eacd6fcc754b1ab6cb5ef6b13b09375aa6
Wael Moharram <wael.moharram@gmail.com>
2023-07-18 16:50:03 +0300

    filters

--- resources/views/dashboard/items/index.blade.php
+++ resources/views/dashboard/items/index.blade.php
@@ -124,7 +124,7 @@
 
                 <td>
                     @foreach(\App\Models\Unit::where('item_id',$row->id)->where('ratio','!=',1)->orderBy('ratio')->get() as $unit)
-                            @if(($loop->index +1) == 1)
+                            @if($loop->index == 0)
                                     <span {{tooltip($unit->name)}}>{{getRound(($amount * (float)$unit->ratio))}}</span>
                                 @else
                             <span {{tooltip($unit->name)}}>{{getRound(($amount * (  (float)$oldUnit->ratio / (float)$unit->ratio)   )  )}}</span>
